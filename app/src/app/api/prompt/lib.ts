25c4e1d7e846d254fbdf8ea8d06d985314561e8e
Shuaib Yunus <shuaibiyy@gmail.com>
2024-06-06 15:47:09 +0200

    Added a basic endpoint and reading from it

---
+++ app/src/app/api/prompt/lib.ts
@@ -0,0 +1,57 @@
+import { readFileSync } from "node:fs";
+import { promisify } from 'util';
+import { message, createDataItemSigner, result } from "@permaweb/aoconnect";
+
+const PROCESS_ID = "1x2lsMZVr67txPJVZ0OQT7qOGYVP-w9EWqcfF57d0Dc";
+
+const wallet = JSON.parse(
+  readFileSync("/Users/ShuaibYunus/.aos.json").toString()
+);
+
+const sleep = promisify(setTimeout);
+
+async function promptModel() {
+ try {
+  const textPrompt =
+    "portrait of a pretty blonde woman, a flower crown, earthy makeup, flowing maxi dress with colorful patterns and fringe, a sunset or nature scene, green and gold color scheme";
+  const negativePrompt = "disfigured, deformed, ugly";
+  const messageId = await message({
+    process: PROCESS_ID,
+    tags: [{ name: "Action", value: "Text-To-Image" }],
+    signer: createDataItemSigner(wallet),
+    data: `{"aiModelID":"096875a5-ed88-47ae-b420-895da26b4c53","params":{"prompt":"${textPrompt}","negative_prompt":"${negativePrompt}","sampler_name":"DPM++ 2M Karras","batch_size":1,"n_iter":1,"steps":50,"cfg_scale":7,"width":512,"height":512}}`,
+  });
+  console.log(`Text-To-Image Message ID: ${messageId}`);
+
+  const { Messages } = await result({
+    message: messageId,
+    process: PROCESS_ID,
+  });
+  // const TASK_ID = "NTKyHWpj";
+  const taskID = JSON.parse(Messages[1].Data).taskID;
+  console.log(`Task ID is ${taskID}`);
+
+  console.log(`Waiting for some seconds...`);
+  await sleep(30000);
+
+  const taskMsgId = await message({
+    process: PROCESS_ID,
+    tags: [{ name: "Action", value: "Get-AI-Task" }],
+    signer: createDataItemSigner(wallet),
+    data: `{"taskID":"${taskID}"}`,
+  });
+  console.log(taskMsgId);
+
+  const { Messages: taskMsg } = await result({
+    message: taskMsgId,
+    process: PROCESS_ID,
+  });
+  console.log(taskMsg);
+
+  const base64Image = JSON.parse(taskMsg[0].Data).ResponseData.images[0];
+
+  return base64Image
+} catch (e) {
+  console.error(e);
+}
+}
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,11 +1,26 @@
-import Image from "next/image";
-import React from 'react';
-import { Button } from 'antd';
+"use client";
 
-const Home = () => (
-  <div className="App">
-    <Button type="primary">Generate Image</Button>
-  </div>
-);
+import Image from "next/image";
+import React from "react";
+import { Button } from "antd";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Fetch the message from the API route
+    fetch("/api/prompt")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching message:", error));
+  }, []);
+
+  return (
+    <div className="App">
+      <Button type="primary">Generate Image</Button>
+    </div>
+  );
+};
 
 export default Home;
